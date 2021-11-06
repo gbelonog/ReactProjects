85b811ae21f5f35bbc108feed82f281a9103ac42
Galina Bilonog <galina.bilonog@ab-soft.net>
2021-11-06 21:24:05 +0200

    added predictions

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  let [isShownLogo, showLogo] = useState( false );
+  let [isShownPrediction, showPrediction] = useState( false );
+  function pushMeButtonHandler(){
+    showLogo(!isShownLogo);
+    showPrediction(!isShownPrediction);
+  }
+  function predictionGenerator(){
+    const randomNumberOfPrediction = Math.floor(Math.random() * 10);
+    const listOfPredictions = {
+      0: "Love",
+      1: "Surprize",
+      2: "Met with friend",
+      3: "Good job",
+      4: "Keep calm and dive",
+      5: "Take it easy",
+      6: "Never give up",
+      7: "Be strong",
+      8: "will be ok",
+      9: "think positive",
+    }
+
+    return listOfPredictions[randomNumberOfPrediction];
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>What will happen to you today?</p>
+        <button onClick = {() => pushMeButtonHandler()}>Push me</button>
+        {!isShownLogo && (<img src={logo} className="App-logo" alt="logo" />)}
+        {isShownPrediction && (<p>prediction {predictionGenerator()}</p>)}
+        
       </header>
     </div>
   );
