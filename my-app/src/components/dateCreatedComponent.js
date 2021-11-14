7be9cb2f041abdd4cc5e8c552f2902de4ed6d8a6
Galina Bilonog <galina.bilonog@ab-soft.net>
2021-11-14 21:51:38 +0200

    corrected date

--- my-app/src/components/dateCreatedComponent.js
+++ my-app/src/components/dateCreatedComponent.js
@@ -3,7 +3,13 @@ import React, { Component } from 'react';
 class DateCreated extends Component {
   render() {
     const { dateCreated, id } = this.props;
-    return <h6 key={"dateCreated"+id}>Date: {dateCreated}</h6>;
+    const monthes = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+    const date = dateCreated.split('T')[0].split('-');
+    return <h6 key={"dateCreated"+id}>Date: {date[2] 
+      + " " + monthes[date[1]-1] 
+      + " " + date[0]
+      + " " + dateCreated.split('T').pop().split("-")[0]
+      }</h6>;
   }
 }
 
