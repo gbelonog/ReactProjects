17a9827e3c64dbf6c59cfd44ff5dd7d55937e88e
Galina Bilonog <galina.bilonog@ab-soft.net>
2021-12-13 18:11:35 +0200

    homeWork5

--- my-app/src/components/NewsForm.js
+++ my-app/src/components/NewsForm.js
@@ -3,6 +3,14 @@ import PropTypes from 'prop-types';
 import faker from 'faker';
 import { getBase64 } from "./utils";
 import { HASHTAGS, AUTHORS } from "../data";
+const ERRORS = {
+  title: "Title cannot be empty.",
+  text: "Text cannot be empty.",
+  photo: "Photo is not selected.",
+  hashTags: "Hash tags are not selected.",
+  author: "Author is not selected.",
+}
+
 
 export class NewsForm extends Component {
   state = {
@@ -12,7 +20,11 @@ export class NewsForm extends Component {
     photo: '',
     hashTags: [],
     author: '',
-    error: null
+    titleError: false,
+    textError: false,
+    photoError: false,
+    hashTagsError: false,
+    authorError: false,
   };
 
   handleSubmit = (e) => {
@@ -22,15 +34,28 @@ export class NewsForm extends Component {
       id,
       ...this.state
     };
-    this.props.onAddNewsItem(news);
+    if(!this.state.title){this.setState({titleError: true})};
+    if(!this.state.text){this.setState({textError: true})};
+    if(!this.state.photo){this.setState({photoError: true})};
+    if(!this.state.author){this.setState({authorError: true})};
+    if(this.state.hashTags.length === 0){this.setState({hashTagsError: true})};
+    if(this.state.titleError 
+      && this.state.textError
+      && this.state.photoError
+      && this.state.authorError
+      && this.state.hashTagsError
+      ){this.props.onAddNewsItem(news);}
+    
   };
 
   handleChangeText = (e) => {
     const input = e.currentTarget;
+    console.log(e.currentTarget);
     const { value, name } = input;
-    this.setState({
-      [name]: value,
-    });
+    this.setState({ [name]: value });
+    if(name !== 'shortDescription'){
+      this.setState({ [name + 'Error']: false });
+    }
   };
 
   handleChangePhoto = (e) => {
@@ -38,6 +63,7 @@ export class NewsForm extends Component {
     getBase64(file, (base64) => {
       this.setState({
         photo: base64,
+        photoError: false
       });
     })
   };
@@ -46,6 +72,7 @@ export class NewsForm extends Component {
     const { value } = e.currentTarget;
     this.setState({
       author: value,
+      authorError: false
     });
   };
 
@@ -61,27 +88,33 @@ export class NewsForm extends Component {
 
     this.setState({
       hashTags: newHashTag,
+      hashTagsError: false
     });
   };
 
   render() {
     const {
-        title,
-        shortDescription,
-        text,
-        photo,
-        hashTags,
-        author,
-        error
+      title,
+      shortDescription,
+      text,
+      photo,
+      hashTags,
+      author,
+      titleError,
+      textError,
+      photoError,
+      hashTagsError,
+      authorError,
     } = this.state;
 
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
             <div>Title:<input value={title} onChange={this.handleChangeText} type="text" name="title" /></div>
-            {error && (<span style={{ color: 'red' }}>{error}</span>)}
+            {titleError && (<span style={{ color: 'red' }}>{ERRORS.title}</span>)}
             <div>Short Description:<textarea value={shortDescription} onChange={this.handleChangeText} name="shortDescription"/></div>
             <div>Text:<textarea value={text} onChange={this.handleChangeText} name="text"/></div>
+            {textError && (<span style={{ color: 'red' }}>{ERRORS['text']}</span>)}
             <div>Photo:
                 {photo.length > 0 && (
                 <img style={{
@@ -92,6 +125,7 @@ export class NewsForm extends Component {
                 )}
                 <input type="file" accept=".jpeg,.png" onChange={this.handleChangePhoto} />
             </div>
+            {photoError && (<span style={{ color: 'red' }}>{ERRORS['photo']}</span>)}
             <div>Author:
                 {AUTHORS.map((e) => (
                     <label key={e}>
@@ -104,6 +138,7 @@ export class NewsForm extends Component {
                     </label>
                 ))}
             </div>
+            {authorError && (<span style={{ color: 'red' }}>{ERRORS['author']}</span>)}
             <div>
                 <span>HashTags:</span>
                 {HASHTAGS.map((e) => (
@@ -117,6 +152,8 @@ export class NewsForm extends Component {
                     </label>
                 ))}
             </div>
+            {hashTagsError && (<span style={{ color: 'red' }}>{ERRORS['hashTags']}</span>)}
+          
             <button type="submit">Create news</button>
         </form>
       </div>
