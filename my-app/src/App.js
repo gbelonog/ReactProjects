7550bf13c23ffc301d10eabdd2c6806c2c4ac485
Galina Bilonog <galina.bilonog@ab-soft.net>
2021-12-20 00:26:49 +0200

    hw

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import StarShips from './StarShips/StarShips';
+
+function App() {
+  return (
+    <div className="App">
+      <StarShips />
+    </div>
+  );
+}
+
+export default App;
